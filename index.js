d5b20cb8b093bb845919f7abac0a9f5e612d2d0a
Mauricio Sganderla <mauricio.webdev@gmail.com>
2016-11-12 23:39:25 -0200

    created conversation flow

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ bot.startRTM(function(err,bot,payload) {
 });
 
 var slackusers = [];
+var userToValidate = {};
+
 function allUsers(fn) {
     bot.api.users.list({},function(err,response){
       fn(response.members);
@@ -32,70 +34,87 @@ var findUserByName = function(username){
     return user.length > 0 ? user[0] : {};
 }
 
+var getUserApps = function(){
 
+  var data = {
+    pos: [
+      {
+        id: 'mauricio.sganderla',
+        apps:[
+          { name: "SAPC"},
+          { name: "CRM"}
+        ]
+      },
+      {
+        id: "gustavo.pacheco",
+        apps:[
+          { name: "SIGMA"},
+          { name: "SGM"}
+        ]
+      }
+    ]
+  };
+  return data;
+}
 
+var getUserByApp = function(appName){
+  return getUserApps().pos.filter(
+    (element) => filterByApp(element,appName)
+  )
+}
+
+var filterByApp = function (element, appName){
+  return element.apps.some( (app) => new RegExp('\\b' + appName + '\\b','i').test(app.name))
+}
 
 
 controller.hears(['RDM FINALIZADA', 'rdm finalizada'], ['mention', 'direct_mention', 'direct_message'], function(bot,message) {
 
+  allUsers( (users) => slackusers = users);
   // start a conversation to handle this response.
-  bot.startConversation(message,function(err,convo) {
-    allUsers( (users) => slackusers = users);
 
-    convo.ask('Qual sistema?',function(response,convo) {
-      var appName = response.text;
-      var data =
-      {
-        pos: [
-          {
-            id: 'schunck',
-            apps:[
-              { name: "SAPC"},
-              { name: "CRM"}
-            ]
-          },
-          {
-            id: "gustavo.pacheco",
-            apps:[
-              { name: "SIGMA"},
-              { name: "SGM"}
-            ]
-          }
-        ]
-      };
+    var askApp = function(err, convo){
+      convo.ask('Qual sistema ?',function(response, convo){
+        var appName = response.text;
+        var poOwned = getUserByApp(appName)
+        var poId = poOwned[0].id;
+        userToValidate = findUserByName(poId)
+        convo.setVar('app', appName);
+        convo.say('Ótimo! Vou procurar quem é o dono...');
 
-      var poOwned = data.pos.filter(
-        (element) => filterByApp(element,appName)
-      )
+        askVersion(response,convo);
 
-      function filterByApp(element, appName){
-        return element.apps.some( (app) => new RegExp('\\b' + appName + '\\b','i').test(app.name))
+        convo.next();
+      });
+
+      var askVersion = function(err, convo){
+        convo.ask('Qual a versão do {{vars.app}} ?',function(response,conv){
+          var version = response.text;
+          convo.setVar('appVersion', version);
+          convo.say('Ótimo, achei!! Já estou indo falar com o '+userToValidate.real_name)
+
+          initializePrivateConversion(response,convo)
+
+          convo.next();
+        })
       }
 
-      var poId = poOwned[0].id;
-      var slackUser = findUserByName(poId)
+      var initializePrivateConversion = function (err, convo){
 
+        var message = 'Olá ' + userToValidate.real_name + '! Precisamos que você valide o {{vars.app}} versão {{vars.appVersion}}'
+        console.log(message);
+        var consversationData = {user:userToValidate.id, message: message }
+        bot.startPrivateConversation(consversationData, function(err,convo){
+          if (err) {
+             console.log(err);
+           } else {
+             convo.say(consversationData.message);
+           }
+         })
+      }
+    }
 
-      convo.say(`Ótimo! Vou falar com o <@${poId}> para validar o  ${appName}`);
-      convo.next();
-
-
-
-      var data = {user:slackUser.id, message:  `Ola! Valida para nos o ${appName}`}
-      bot.startPrivateConversation(data, function(err,convo){
-        if (err) {
-                 console.log(err);
-               } else {
-                 convo.say(data.message);
-               }
-      })
-
-
-
-
-
-    });
-  })
+    bot.startConversation(message, askApp);
 
 
 
